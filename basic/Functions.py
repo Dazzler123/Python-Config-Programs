434b4bda57b83be11b030cebfb86529c74e8d823
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-23 21:26:58 +0530

    Implemented Dice number generator

--- basic/Functions.py
+++ basic/Functions.py
@@ -7,3 +7,15 @@ def hashtagGen(text):
     return "#" + s1
 
 print(hashtagGen(s))
+
+
+# random module - dice number generator
+import random
+random.seed(int(input())) # initialize random & take user input
+
+#generate the random values for every dice
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+
+print(dice1)
+print(dice2)
