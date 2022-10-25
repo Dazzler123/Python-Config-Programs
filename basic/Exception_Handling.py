909f4c8c9bd033f03b1a0befa87c2276b0219060
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-25 21:14:00 +0530

    Content updated

--- basic/Exception_Handling.py
+++ basic/Exception_Handling.py
@@ -23,32 +23,3 @@ finally:
 	# and finally here
 	print("Have a good day")
 # //////////////////////////////////////////////////////////
-
-
-# File I/O ---> Text file reading program
-file = open("filename.txt")
-n = int(input())
-
-#your code goes here
-resul = file.readlines()
-print(resul[n])
-file.close()
-# //////////////////////////////////////////////////////////
-
-# File I/O ---> Text file writing program
-names = ["John", "Oscar", "Jacob"]
-file = open("names.txt", "w+")
-#write down the names into the file
-file.write(names[0])
-file.write(names[1])
-file.write(names[2])
-file.close()
-
-file= open("names.txt", "r")
-#output the content of file in console
-resul = file.readlines()
-print(resul[1])
-print(resul[2])
-print(resul[3])
-file.close()
-# //////////////////////////////////////////////////////////
