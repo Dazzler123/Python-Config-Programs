7f140508252cb2dc54ebab0b591205f399c29dae
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-25 20:50:41 +0530

    Implemented Textfile writing program

--- basic/Exception_Handling.py
+++ basic/Exception_Handling.py
@@ -34,3 +34,18 @@ resul = file.readlines()
 print(resul[n])
 file.close()
 # //////////////////////////////////////////////////////////
+
+# File I/O ---> Text file writing program
+names = ["John", "Oscar", "Jacob"]
+file = open("names.txt", "w+")
+#write down the names into the file
+file.write(names[0])
+file.write(names[1])
+file.write(names[2])
+file.close()
+
+file= open("names.txt", "r")
+#output the content of file in console
+print(file.readline())
+file.close()
+# //////////////////////////////////////////////////////////
