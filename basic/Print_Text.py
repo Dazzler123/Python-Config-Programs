47cf38a9cd919f7e74f215da146848e70b778cbd
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-26 21:51:45 +0530

    Content updated

--- basic/Print_Text.py
+++ basic/Print_Text.py
@@ -1,7 +1,7 @@
-print("Hello");
-print("There");
-print("I am");
-print("here");
-print("to");
-print("learn");
-print("Python");
+print("Hello")
+print("There")
+print("I am")
+print("here")
+print("to")
+print("learn")
+print("Python")
